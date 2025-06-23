df793ddd9c293eb10bf206416262a105e4352e80
Jonnie Barnsley <jonniebarnsley@gmail.com>
2025-06-23 09:22:02 +0100

    exploring sobol sequences as a sampling method to replace latin hypercubes

---
+++ R/Sobol.R
@@ -0,0 +1,11 @@
+install.packages("randtoolbox")
+library(randtoolbox)
+
+sample = sobol(n=1024, dim=8, start=0)
+start <- 256
+end <- start + 16
+subsample <- sample[start:end,]
+#plot(subsample)
+
+pairs(subsample)
+
