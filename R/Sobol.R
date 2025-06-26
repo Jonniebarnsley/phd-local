6cb53adcae84a6c9a6cda816068a128155c750bd
Jonnie Barnsley <jonniebarnsley@gmail.com>
2025-06-26 10:57:51 +0100

    generates sobol sequence for ensemble

--- R/Sobol.R
+++ R/Sobol.R
@@ -1,7 +1,7 @@
 install.packages("randtoolbox")
 library(randtoolbox)
 
-sample = sobol(n=1024, dim=8, start=0)
+sample = sobol(n=1024, dim=6, start=0)
 start <- 256
 end <- start + 16
 subsample <- sample[start:end,]
